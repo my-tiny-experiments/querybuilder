378be7914f5727b884d6709ae30405811449633b
Hassan Salem <h.salem7788@gmail.com>
2018-10-08 13:29:17 +0300

    add support for nested queries [ the code need improvements, but this is a proof of concept ]

--- src/Query.php
+++ src/Query.php
@@ -16,9 +16,30 @@ class Query
     {
         $query = $query? $query: $model->newQuery();
         $filterable = $model->filterable;
+        $subFilters = $model->subFilters;
 
         $filter = new Filter($filterable);
         $rules = $filter->getAllowedRules($filters);
+
+        foreach($rules['rel'] ?? [] as $key => $value) {
+            foreach($value as $subKey => $subValue) {
+                if($subValue['data']['nested']?? false == 'true') {
+                    $subModel = $subFilters[$key];
+                    $subQuery = self::build(new $subModel(), $subValue['value']);
+                    $rules['rel'][$key][$subKey]['value'] = $subQuery->get()->pluck('id')->toArray();
+                }
+            }
+        }
+
+        foreach($rules['notRel'] ?? [] as $key => $value) {
+            foreach($value as $subKey => $subValue) {
+                if($subValue['data']['nested']?? false == 'true') {
+                    $subModel = $subFilters[$key];
+                    $subQuery = self::build(new $subModel(), $subValue['value']);
+                    $rules['notRel'][$key][$subKey]['value'] = $subQuery->get()->pluck('id')->toArray();
+                }
+            }
+        }
 
         $newQuery = new QueryBuilder($query, $filterable);
         return $newQuery->buildQuery($rules);
